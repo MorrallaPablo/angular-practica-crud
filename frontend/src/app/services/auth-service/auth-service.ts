4dedbe67b054b718162eaeee0bbd0057bb970528
Pablo Morralla <pmorrall@emeal.nttdata.com>
2026-04-30 10:23:13 +0200

    Made it so that AuthService's login updates the token while returning an observable for error handling

--- frontend/src/app/services/auth-service/auth-service.ts
+++ frontend/src/app/services/auth-service/auth-service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginDto } from '../../../model/DTO/login-dto';
 import { LoginResponseDto } from '../../../model/DTO/login-response-dto';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -28,17 +29,17 @@ export class AuthService {
     return !!this.getToken();
   }
 
-  login(login: LoginDto) {
-    this.http.post<LoginResponseDto>(
+  login(login: LoginDto): Observable<LoginResponseDto> {
+    return this.http.post<LoginResponseDto>(
       `${this.baseUrl}/auth/login`,
       {
         email: login.email,
         password: login.password
       }
     ).pipe(
-      (loginResponse) => {
+      tap(loginResponse => {
         this.setToken(loginResponse.access_token);
-      }
-    )
+      })
+    );
   }
 }
